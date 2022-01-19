c7dd63bef8e0b1b703cb279646211079ea0de21e
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-01-19 12:29:12 +0530

    Time: 397 ms (40.69%), Space: 58.5 MB (70.27%) - LeetHub

---
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+     vector<int> dir = { 0, 1, 0, -1, 0 }; 
+    bool isCyclic(vector<vector<char>>& grid, vector<vector<bool>>& visited, int i, int j, int x, int y)
+    {
+        visited[i][j] = true;
+        for(int d = 0; d < 4; ++d)
+        {
+            int a = i+dir[d];
+            int b = j+dir[d+1];
+            if(a >= 0 && a < grid.size() && b >= 0 && b < grid[0].size() && grid[a][b] == grid[i][j] && !(x == a && y == b))
+                if(visited[a][b] || isCyclic(grid, visited, a,b,i,j))
+                    return true;
+        }
+        return false;
+    } 
+    bool containsCycle(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j]&&isCyclic(grid,vis,i,j,-1,-1)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
