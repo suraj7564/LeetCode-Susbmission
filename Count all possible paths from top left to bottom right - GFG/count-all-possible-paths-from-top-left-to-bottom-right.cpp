e855b5693f4350f381c3181e24f5f6541b6f419f
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-02-12 09:02:55 +0530

    Added solution - LeetHub

---
+++ Count all possible paths from top left to bottom right - GFG/count-all-possible-paths-from-top-left-to-bottom-right.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long int numberOfPaths(int m, int n){
+        // code here
+        long long mod=1e9+7;
+        long long dp[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0||j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    dp[i][j]=(dp[i][j-1]+dp[i-1][j])%mod;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> m >> n;
+        Solution ob;
+        cout<<ob.numberOfPaths(m,n)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
