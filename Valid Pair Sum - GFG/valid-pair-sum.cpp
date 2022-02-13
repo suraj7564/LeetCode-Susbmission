e2da6c276b5090e5004b6ef0e73e763be87786fa
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-02-13 22:16:25 +0530

    Added solution - LeetHub

---
+++ Valid Pair Sum - GFG/valid-pair-sum.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+class Solution{
+    
+    public:
+    long long ValidPair(int a[], int n) 
+    { 
+    	sort(a,a+n);
+    	long long ans=0,cnt=0;
+    	for(int i=0;i<n;i++){
+    	    if(a[i]>0){
+    	        cnt++;
+    	    }
+    	    else{
+    	        int idx=upper_bound(a,a+n,abs(a[i]))-a;
+    	        ans+=n-idx;
+    	    }
+    	}
+    	//cout<<ans<<" ";
+    	long long total=(cnt*(cnt-1))/2;
+    	return ans+total;
+    }   
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int array[n];
+		for (int i = 0; i < n; ++i)
+			cin>>array[i];
+	    Solution obj;
+		cout << obj.ValidPair(array, n) <<"\n";
+	}
+	return 0; 
+} 
+
+  // } Driver Code Ends
