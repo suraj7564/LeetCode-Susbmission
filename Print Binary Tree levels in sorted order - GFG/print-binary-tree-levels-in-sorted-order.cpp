48df9308473c3915013e6a7ca396db5f153ca0ad
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-02-13 20:45:42 +0530

    Added solution - LeetHub

---
+++ Print Binary Tree levels in sorted order - GFG/print-binary-tree-levels-in-sorted-order.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+class Solution
+{
+    public:
+    vector <vector <int>> binTreeSortedLevels (int arr[], int n)
+    {
+        vector<vector<int>> ans;
+        int cur=1;
+        for(int i=0;i<n;){
+            vector<int> cc;
+            int j=0;
+            while(j<cur&&i<n){
+                cc.push_back(arr[i]);
+                j++,i++;
+            }
+            cur=cur*2;
+            sort(cc.begin(),cc.end());
+            ans.push_back(cc);
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		int n; cin >> n;
+		int arr[n];
+		for (int i = 0; i < n; ++i)
+			cin >> arr[i];
+
+        Solution ob;
+        
+		vector <vector <int>> res = ob.binTreeSortedLevels (arr, n);
+		for (int i = 0; i < res.size(); ++i)
+		{
+			for (int j = 0; j < res[i].size(); ++j)
+			{
+				cout << res[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+}
+
+// Contributed By: Pranay Bansal  // } Driver Code Ends
