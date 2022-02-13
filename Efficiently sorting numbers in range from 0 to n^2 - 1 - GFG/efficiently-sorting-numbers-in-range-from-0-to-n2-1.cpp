58acc1617a3e76e31b44b8946fbe189856e50e8d
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-02-13 22:10:34 +0530

    Added solution - LeetHub

--- Efficiently sorting numbers in range from 0 to n^2 - 1 - GFG/efficiently-sorting-numbers-in-range-from-0-to-n2-1.cpp
+++ Efficiently sorting numbers in range from 0 to n^2 - 1 - GFG/efficiently-sorting-numbers-in-range-from-0-to-n2-1.cpp
@@ -7,9 +7,26 @@ using namespace std;
 
 class Solution{
     public:
+    void countSort(int arr[],int n,int ex){
+        vector<int> count(n,0),ans(n,0);
+        for(int i=0;i<n;i++){
+            count[(arr[i]/ex)%n]++;
+        }
+        for(int i=1;i<n;i++){
+            count[i]+=count[i-1];
+        }
+        for(int i=n-1;i>=0;i--){
+            ans[count[(arr[i]/ex)%n]-1]=arr[i];
+            count[(arr[i]/ex)%n]--;
+        }
+        for(int i=0;i<n;i++){
+            arr[i]=ans[i];
+        }
+    }
     void sort(int arr[], int n)
     {
-        stable_sort(arr,arr+n);
+        countSort(arr,n,1);
+        countSort(arr,n,n);
     }
 };
 
