5f2ee61e7246a5e27ed217d7a89c55453578cf4e
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-02-15 23:09:01 +0530

    Added solution - LeetHub

---
+++ Part of it. - GFG/part-of-it..cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+//Initial template for c++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    string isPart(int n) { 
+        //complete the function here
+        int x=n+2;
+        for(int i=2;i<x;i++){
+            if(x%i==0){
+                return "No";
+            }
+        }
+        return "Yes";
+    }
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.isPart(n) << endl;
+    
+    }
+    return 0; 
+} 
+  // } Driver Code Ends
