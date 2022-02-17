ae812053bf72849fe95881f0b0807616534c82d1
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-02-17 20:55:12 +0530

    Time: 145 ms (17.84%), Space: 37.7 MB (22.14%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    void find(vector<int>& arr,int st,int cursum,int target,vector<vector<int>>& ans,vector<int>& cur){
-        if(cursum>target) return;
-        if(cursum==target){
-            ans.push_back(cur);
-            return;
-        }
-        for(int i=st;i<arr.size();i++){
-            cur.push_back(arr[i]);
-            find(arr,i,cursum+arr[i],target,ans,cur);
-            cur.pop_back();
-        }
-    }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+   void find(vector<int> ca,int curpos,int cursum,int target,vector<vector<int>>& ans,vector<int>& cur){
+       if(cursum>target){
+           return;
+       }
+       if(cursum==target){
+           ans.push_back(cur);
+           return;
+       }
+       for(int i=curpos;i<ca.size();i++){
+           cur.push_back(ca[i]);
+           find(ca,i,cursum+ca[i],target,ans,cur);
+           cur.pop_back();
+       }
+   }
+    vector<vector<int>> combinationSum(vector<int>& c, int t) {
         vector<vector<int>> ans;
         vector<int> cur;
-        find(candidates,0,0,target,ans,cur);
+        find(c,0,0,t,ans,cur);
         return ans;
     }
 };
