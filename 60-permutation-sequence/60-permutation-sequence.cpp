c2e8d1f9a080d5309ccdb739bbced6ff2a9c6a31
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-04-21 13:52:47 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (18.37%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    void solve(vector<int>& v,int total,int k,int n,string &ans){
-        if(n<=0) return;
-        ans += to_string(v[k/total]);
-        v[k/total] = 20;
-        sort(v.begin(),v.end());
-        k = k%total;
-        if(n-1>0){
-            total/=(n-1);
-        }
-        solve(v,total,k,n-1,ans);
-    }
     string getPermutation(int n, int k) {
         vector<int> v;
-        int total = 1;
+        int fact = 1;
         for(int i=1;i<=n;i++){
             v.push_back(i);
             if(i!=1)
-            total = total*(i-1);
+            fact = fact*(i-1);
         }
         k-=1;
         string ans = "";
-        solve(v,total,k,n,ans);
+        while(true){
+            ans += to_string(v[k/fact]);
+            v.erase(v.begin()+k/fact);
+            if(v.size()==0) break;
+            k = k%fact;
+            fact = fact/v.size();
+        }
         return ans;
     }
 };
