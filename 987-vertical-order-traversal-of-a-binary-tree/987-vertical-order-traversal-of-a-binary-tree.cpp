d3c5686d65046334cd69375ca74104045b50f0fe
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-05-01 11:47:51 +0530

    Time: 3 ms (91.07%), Space: 12 MB (83.16%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,41 +11,33 @@
  */
 class Solution {
 public:
-    int l=100000,r1=0;
-    map<pair<int,int>,vector<int> > m;
-    void find(TreeNode* root,int r,int c){
-        if(!root) return;
-        m[{r,c}].push_back(root->val);
-        l=min(l,c);
-        r1=max(r1,c);
-        //cout<<r<<" "<<c<<" "<<r1<<" "<<l<<endl;
-        find(root->left,r+1,c-1);
-        find(root->right,r+1,c+1);
-    }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        l=100000,r1=0;
-        find(root,0,0);
-        //cout<<r1<<" "<<l<<endl;
-        vector<vector<int>> ans(r1-l+1);
-        for (auto &[i, j]: m) {
-            sort(j.begin(), j.end());
-            ans[i.second - l].push_back(i.first); //push each rwo
-        }
-        
-        int col = l;
-        for (vector<int> &v: ans) {
-            sort(v.begin(), v.end()); //sort all rows
-            vector<int> tmp = v;
-            v.clear();
-            
-            //push sorted rows for each column in ans
-            for (int r: tmp) {
-                // v.push_back(mp[{r, col}]);
-                for (int x: m[{r, col}])
-                    v.push_back(x);
+        if(!root) return {};
+        vector<vector<int>> ans;
+        queue<pair<TreeNode*,int>> q;
+        q.push({root,0});
+        map<int,vector<int>> m;
+        while(!q.empty()){
+            int sz = q.size();
+            vector<pair<int,int>> temp;
+            while(sz-->0){
+                auto curNode = q.front().first;
+                int line = q.front().second;
+                q.pop();
+                temp.push_back({curNode->val,line});
+                if(curNode->left) q.push({curNode->left,line-1});
+                if(curNode->right) q.push({curNode->right,line+1});
             }
-            ++col;
-        }        
-        return ans;
+            sort(temp.begin(),temp.end());
+            for(auto x:temp){
+                m[x.second].push_back(x.first);
+            }
+        }
+        for(auto x:m){
+            vector<int> temp = x.second;
+            //sort(temp.begin(),temp.end());
+            ans.push_back(temp);
+        }
+    	return ans;
     }
 };
