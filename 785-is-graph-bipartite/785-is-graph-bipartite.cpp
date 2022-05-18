4fda015b5c3511b966344b6cbca551e331fdb823
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-05-19 01:13:19 +0530

    Time: 20 ms (96.65%), Space: 13.7 MB (37.27%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    bool b = true;;
-    void dfs(vector<vector<int>>& g,int cur,int papa,int curColor,vector<int>& vis){
-        curColor = (curColor+1)%2;
-        vis[cur] = curColor;
-        for(auto x:g[cur]){
-            //cout<<x<<" "<<cur<<endl;
-            if(vis[x]==-1){
-                dfs(g,x,cur,curColor,vis);
-                //else return false;
-            }
-            else if(vis[x]==vis[cur]){
-                //cout<<x<<" "<<papa<<" "<<v[x]<<endl;
-                b = false;
-            }
-        }
-    }
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> vis(n,-1);
-        b = true;
+        vector<int> color(n,0);
         for(int i=0;i<n;i++){
-            if(vis[i]==-1){
-                dfs(graph,i,-1,0,vis);
+            if(color[i]==0){
+                queue<int> q;
+                q.push(i);
+                color[i] = 1;
+                while(!q.empty()){
+                    int cur = q.front();
+                    q.pop();
+                    for(auto x:graph[cur]){
+                        if(color[x]==0){
+                            color[x]=-color[cur];
+                            q.push(x);
+                        }
+                        else if(color[x]==color[cur]) return false;
+                    }
+                }
             }
         }
         
-        // for(int i=0;i<n;i++){
-        //     cout<<vis[i]<<" ";
-        // }
-        // cout<<endl;
-        return b;
+        return true;
     }
 };
