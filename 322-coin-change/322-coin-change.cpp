92e30c8b14ce8c5b931f1a99c1940ae4eb9ab83b
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-05-24 18:27:01 +0530

    Time: 96 ms (71.87%), Space: 18.4 MB (26.87%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -4,15 +4,9 @@ public:
         vector<double> dp(amount+1, INT_MAX);
         dp[0] = 0;
         
-        // dp[i] represents the fewest number of coins to make up the amount i
-        for (int i = 1; i <= amount; i++) {
-            
-            for (auto coin : coins) {
-                
-                if (coin <= i) {
-                    
-                    dp[i] = min(dp[i], dp[i-coin]+1);
-                }
+        for(auto coin : coins){
+            for(int i=coin;i<=amount;i++){
+                dp[i] = min(dp[i],dp[i-coin]+1);
             }
         }
         
