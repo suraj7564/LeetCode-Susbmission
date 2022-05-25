8b539afef12385b0a420d296c838a8cabbfafda1
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-05-25 18:25:50 +0530

    Added solution - LeetHub

--- Longest Palindromic Substring - GFG/longest-palindromic-substring.cpp
+++ Longest Palindromic Substring - GFG/longest-palindromic-substring.cpp
@@ -10,29 +10,26 @@ using namespace std;
 class Solution{   
 public:
     string longestPalindrome(string s){
-        int n=s.length();
-        vector<vector<int> > dp(n+1,vector<int>(n+1,0));
-        string res="";
-        res+=s[0];
-        int ans=0;
-        int l=0,r=0;
+        // code here 
+        string ans = "";
+        ans += s[0];
+        int l = 0,r = 0;
+        int n = s.length();
+        vector<vector<bool>> dp(n+1,vector<bool>(n+1,false));
         for(int i=0;i<=n;i++){
-            dp[i][0]=dp[i][1]=1;
+            dp[i][0] = dp[i][1] = true;
         }
         for(int i=2;i<=n;i++){
             for(int j=i;j<=n;j++){
-                if(s[j-1]==s[j-i]&&dp[j-1][i-2]==1){
-                    dp[j][i]=1;
-                    l=j-i;
-                    r=j-1;
-                    if((r-l+1)>res.length())
-                    res=s.substr(l,r-l+1);
+                if(s[j-1]==s[j-i]&&dp[j-1][i-2]){
+                    dp[j][i] = true;
+                    if(i>ans.length())
+                    ans = s.substr(j-i,i);
                 }
-                else dp[j][i]=0;
+                else dp[j][i] = false;
             }
         }
-        
-        return res;
+        return ans;
     }
 };
 
