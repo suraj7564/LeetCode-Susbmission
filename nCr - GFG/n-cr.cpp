d4b2dfa304c43f945b9f73ff5f700bffa1316f75
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-05-26 15:29:31 +0530

    Added solution - LeetHub

--- nCr - GFG/n-cr.cpp
+++ nCr - GFG/n-cr.cpp
@@ -1,55 +1,44 @@
 // { Driver Code Starts
-//Initial template for c++
+// Initial Template for C++
 
-#include<bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
  // } Driver Code Ends
-//User function Template for C++
+// User function Template for C++
 
-class Solution{   
+class Solution{
 public:
-    long long mod=1e9+7;
-    long long power(long long a,long long b){
-        long long res=1LL;
-        while(b){
-            if(b&1){
-                res=(res*a)%mod;
-                b--;
+    int nCr(int n, int r){
+        if(r>n) return 0;
+        r = min(r,n-r);
+        int mod = 1e9+7;
+        vector<vector<int>> dp(n+1,vector<int>(r+1,0));
+        dp[0][0] = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=min(i,r);j++){
+                if(j==0||j==i) dp[i][j] = 1;
+                else{
+                    dp[i][j] = (dp[i-1][j-1]%mod+dp[i-1][j]%mod)%mod;
+                }
             }
-            a=(a*a)%mod;
-            b=b>>1;
         }
-        return res;
-    }
-    long int ncr(int n, int k){
-        if(n<k) return 0;
-        long long ans=1;
-        if(k>n-k){
-            k=n-k;
-        }
-        for(long long i=1;i<=k;i++){
-            ans=(ans*(n-i+1))%mod;
-            ans=(ans*power(i,mod-2))%mod;
-        }
-        return ans;
+        return dp[n][r];
+        // code here
     }
 };
 
 // { Driver Code Starts.
 
-int main() 
-{ 
+int main(){
     int t;
     cin>>t;
-    while(t--)
-    {
+    while(t--){
         int n, r;
-        cin >> n >> r;
+        cin>>n>>r;
+        
         Solution ob;
-        cout << ob.ncr(n, r) << endl;
-    
+        cout<<ob.nCr(n, r)<<endl;
     }
-    return 0; 
-} 
-  // } Driver Code Ends
+    return 0;
+}  // } Driver Code Ends
