5c9a5266b4a9234c201cb54c03c0eeeb65537edf
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-05-27 16:20:12 +0530

    Added solution - LeetHub

---
+++ IPL 2021 - Match Day 2 - GFG/ipl-2021-match-day-2.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+        // your code here
+        priority_queue<pair<int,int>> pq;
+        vector<int> ans;
+        for(int i=0;i<k-1;i++){
+            pq.push({arr[i],i});
+        }
+        for(int i=k-1;i<n;i++){
+            pq.push({arr[i],i});
+            while(pq.size()>k&&pq.top().second<=i-k) pq.pop();
+            ans.push_back(pq.top().first);
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution ob;
+        vector<int> res = ob.max_of_subarrays(arr, n, k);
+        for (int i = 0; i < res.size(); i++) cout << res[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}  // } Driver Code Ends
