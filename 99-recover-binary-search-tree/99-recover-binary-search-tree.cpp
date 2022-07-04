df9d6f5f8dab44f66abb11b4f027ac3a3af0e783
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-07-04 12:51:35 +0530

    Time: 49 ms (58.36%), Space: 57.8 MB (69.11%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -12,18 +12,18 @@
 class Solution {
 public:
     void markTree(TreeNode* root, TreeNode*& prev, TreeNode*& first, TreeNode*& end) {
-        if (!root) return;
-        markTree(root->left, prev, first, end);
-        if (prev) {
-            if (root->val < prev->val) {
-                if (!first) {
+        if(!root) return;
+        markTree(root->left,prev,first,end);
+        if(prev){
+            if(prev->val>root->val){
+                if(!first){
                     first = prev;
                 }
                 end = root;
             }
         }
         prev = root;
-        markTree(root->right, prev, first, end);
+        markTree(root->right,prev,first,end);
     }
     void recoverTree(TreeNode* root) {
         TreeNode *prev = nullptr, *first = nullptr, *end = nullptr;
