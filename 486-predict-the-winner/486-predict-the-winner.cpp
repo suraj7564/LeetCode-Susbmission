f576e96c6dd5f3659ea13bab2fc8896b6347d21a
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-07-05 11:48:42 +0530

    Time: 5 ms (55.76%), Space: 7.7 MB (23.83%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int find(int l,int r,vector<int>& nums){
+    int find(int l,int r,vector<int>& nums,vector<vector<int>>& dp){
         if(l>=nums.size()||r<0||l>r) return 0;
 
-        if(l==r) return nums[l];
-        if(l+1==r) return max(nums[l],nums[r]);
+        if(dp[l][r]!=-1) return dp[l][r];
         
-        return max(nums[l]+min(find(l+1,r-1,nums),find(l+2,r,nums)),
-                               nums[r]+min(find(l+1,r-1,nums),find(l,r-2,nums)));
+        if(l==r) return dp[l][r] = nums[l];
+        if(l+1==r) return dp[l][r] = max(nums[l],nums[r]);
+        
+        return dp[l][r] = max(nums[l]+min(find(l+1,r-1,nums,dp),find(l+2,r,nums,dp)),
+                               nums[r]+min(find(l+1,r-1,nums,dp),find(l,r-2,nums,dp)));
     }
     bool PredictTheWinner(vector<int>& nums) {
         int total = 0;
@@ -15,9 +17,10 @@ public:
             total += x;
         }
         int n = nums.size();
-        int player1 = find(0,n-1,nums);
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        int player1 = find(0,n-1,nums,dp);
         int player2 = total-player1;
-        cout<<player1<<" "<<player2<<endl;
+        
         return player1>=player2;
     }
 };
