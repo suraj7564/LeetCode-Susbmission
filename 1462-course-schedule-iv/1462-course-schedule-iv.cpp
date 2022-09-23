3a84511218d874715942df1f9f437eea097ae754
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-09-24 01:12:52 +0530

    Time: 1249 ms (5.07%), Space: 201.9 MB (5.06%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> get(vector<vector<int>>& pre, int n){
-        vector<int> in(n, 0);
-        vector<int> adj[n];
+    vector<int> adj[1005];
+    bool canReach(int s, int d, int n,vector<int> adj[]){
+        vector<int> vis(n, 0);
+        queue<int> q;
+        q.push(s);
+        vis[s] = 1;
+        while(!q.empty()){
+            auto t = q.front();
+            if(t == d){
+                return true;
+            }
+            q.pop();
+            for(auto x:adj[t]){
+                if(vis[x] == 0){
+                    vis[x] = 1;
+                    q.push(x);
+                }
+            }
+        }
+        return false;
+    }
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& pre, vector<vector<int>>& queries) {
         for(auto x:pre){
             adj[x[1]].push_back(x[0]);
-            in[x[0]]++;
         }
-        vector<vector<int>> cur(n, vector<int>(n, 0));
-        queue<int> q;
-        for(int i=0;i<n;i++){
-            if(in[i] == 0){
-                q.push(i);
-            }
-        }
-        while(!q.empty()){
-            int v = q.front();
-            q.pop();
-            for(auto u:adj[v]){
-                for(int i=0;i<n;i++){
-                    if(cur[v][i]) cur[u][i] = 1; 
-                }
-                cur[u][v] = 1;
-                if(--in[u] == 0){
-                    q.push(u);
-                }
-            }
-        }
-        return cur;
-    }
-    
-    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        vector<vector<int>> topo = get(prerequisites, n);
         vector<bool> ans;
         for(auto x:queries){
             int u = x[0], v = x[1];
-            
-            if(topo[u][v] == 1) ans.push_back(true);
+            //cout<<u<<" "<<v<<" "<<topo[u]<<" "<<topo[v]<<", ";
+            if(canReach(v, u, n, adj)) ans.push_back(true);
             else ans.push_back(false);
         }
         return ans;
