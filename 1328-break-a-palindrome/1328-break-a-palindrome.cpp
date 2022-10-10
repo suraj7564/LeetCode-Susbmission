0acf3918c3a5a864a045d3951e5bb54ccef83469
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-10-10 14:46:24 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.51%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string breakPalindrome(string s) {
+        if(s.length()==1) return "";
+        for(int i=0;i<s.length()/2;i++){
+            if(s[i]!='a'){
+                s[i]='a';
+                return s;
+            }
+        }
+        s[s.length()-1]='b';
+        return s;
+    }
+};
