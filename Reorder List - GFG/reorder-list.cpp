c75f77cb54f9443f6a20e8a347ad09b94c803b4b
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-10-12 19:52:47 +0530

    Added solution - LeetHub

---
+++ Reorder List - GFG/reorder-list.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void reorderList(struct Node* head);
+
+/* Function to create a new Node with given data */
+struct Node* newNode(int data) {
+    struct Node* new_Node = new Node(data);
+    new_Node->data = data;
+    new_Node->next = NULL;
+    return new_Node;
+}
+
+void printList(struct Node* Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+
+void freeList(struct Node* head) {
+    struct Node* temp;
+    while (head != NULL) {
+
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main(void) {
+    int t, n, m, i, x;
+
+    cin >> t;
+    while (t--) {
+        struct Node* temp, *head;
+        cin >> n;
+        cin >> x;
+        head = new Node(x);
+        temp = head;
+        for (i = 0; i < n - 1; i++) {
+            cin >> x;
+            temp->next = new Node(x);
+            temp = temp->next;
+        }
+
+        reorderList(head);
+        printList(head);
+        freeList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Following is the Linked list node structure */
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+#include<bits/stdc++.h>
+void reorderList(Node* head) {
+    // Your code here
+    vector<Node*> cur;
+    while(head){
+        cur.push_back(head);
+        head = head->next;
+    }
+    Node *ans = new Node(0);
+    Node *res = ans;
+    int i = 0, j = cur.size()-1;
+    while(i<=j){
+        ans->next = cur[i];
+        ans = ans->next;
+        if(i != j){
+            ans->next = cur[j];
+            ans = ans->next;
+        }
+        i++, j--;
+    }
+    ans->next = NULL;
+    head = res->next;
+}
