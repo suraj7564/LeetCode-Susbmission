cfc07b65eea957bb9a6306d0ab3af94bb2662b37
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-10-13 18:41:49 +0530

    Added solution - LeetHub

---
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+  public:
+	int minDifference(int arr[], int n)  { 
+	    // Your code goes here
+	    int sum = 0;
+	    for(int i=0;i<n;i++){
+	        sum += arr[i];
+	    }
+	    vector<int> dp(sum+1, 0);
+	    dp[0] = 1;
+	    for(int i=0;i<n;i++){
+	        for(int j=sum;j>=arr[i];j--){
+	            dp[j] |= dp[j-arr[i]];
+	        }
+	    }
+	    int ans = 1e9;
+	    for(int i=0;i<=sum;i++){
+	        if(dp[i])
+	            ans = min(ans, abs(sum - 2*i));
+	    }
+	    return ans;
+	} 
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.minDifference(a, n) << "\n";
+	     
+    }
+    return 0;
+}
+// } Driver Code Ends
