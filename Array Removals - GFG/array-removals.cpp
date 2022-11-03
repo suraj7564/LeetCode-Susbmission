fc658309e59a7d92e63a54800c7be1727850a9e6
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-11-03 11:15:50 +0530

    Added solution - LeetHub

--- Array Removals - GFG/array-removals.cpp
+++ Array Removals - GFG/array-removals.cpp
@@ -1,31 +1,30 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 //Initial Template for C++
 
 #include<bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function Template for C++
 
 class Solution{
     public:
-    int removals(vector<int>& a, int k){
+    int removals(vector<int>& arr, int k){
         //Code here
-        sort(a.begin(),a.end());
-        int n = a.size();
-        int ans = n;
-        for(int i=0;i<n;i++){
-            int cur = a[i]+k;
-            int idx = upper_bound(a.begin(),a.end(),cur)-a.begin();
-            ans = min(ans,n-(idx-i));
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        int ans = arr.size();
+        for(int i=0;i<arr.size();i++){
+            int idx = upper_bound(arr.begin(), arr.end(), arr[i]+k)-arr.begin();
+            ans = min(ans, i + n - idx);
         }
         return ans;
     }
 };
 
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 
 int main(){
@@ -35,17 +34,18 @@ int main(){
     while(t--){
         int n,k;
         cin>>n>>k;
-        vector<int> a(n);
+        vector<int> arr(n);
         for(int i=0;i<n;i++){
-            cin>>a[i];
+            cin>>arr[i];
         }
         
         Solution ob;
-        int ans = ob.removals(a,k);
+        int ans = ob.removals(arr,k);
         
         cout<<ans<<endl;
     }
 }
 
 
-  // } Driver Code Ends
+
+// } Driver Code Ends
