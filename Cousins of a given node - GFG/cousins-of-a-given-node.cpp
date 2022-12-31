e357c7322645cdaa2b5cd341c207815b7c7c8e8a
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2022-12-31 10:35:28 +0530

    Added solution - LeetHub

---
+++ Cousins of a given node - GFG/cousins-of-a-given-node.cpp
@@ -0,0 +1,194 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Binary Tree Node
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+	Node* temp = new Node;
+	temp->data = val;
+	temp->left = NULL;
+	temp->right = NULL;
+
+	return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node* root = newNode(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current node
+			currNode->left = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+Node* pointer(Node* root, int n)
+{
+	if (root == NULL)
+		return NULL;
+
+	if (root->data == n)
+		return root;
+
+	Node* l = pointer(root->left, n);
+	if (l != NULL && l->data == n)
+		return l;
+
+	Node* r = pointer(root->right, n);
+	return r;
+
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+// A Binary Tree Node
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
+};*/
+
+class Solution
+{
+    public:
+    map<Node*, Node*> par;
+    int lvl = 0;
+    void dfs(Node* root, Node* p, Node* node, int l){
+        if(!root) return;
+        
+        par[root] = p;
+        if(root == node){
+            lvl = l;
+        }
+        
+        dfs(root->left, root, node, l + 1);
+        dfs(root->right, root, node, l + 1);
+    }
+    void get(Node* root, int l, Node* node, vector<int>& ans){
+        if(!root) return;
+        
+        if(l == lvl && par[root] != par[node]){
+            ans.push_back(root->data);
+        }
+        
+        get(root->left, l + 1, node, ans);
+        get(root->right, l + 1, node, ans);
+    }
+    vector<int> printCousins(Node* root, Node* node)
+    {
+        //code here
+        if(!root) return {-1};
+        dfs(root, NULL, node, 0);
+        vector<int> ans;
+        get(root, 0, node, ans);
+        if(ans.size() > 0) return ans;
+        return {-1};
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin >> t;
+	getchar();
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		getchar();
+
+		string inp;
+		getline(cin, inp);
+		
+		struct Node* root = buildTree(inp);
+
+		Node* p = pointer(root, n);
+
+        Solution ob;
+		vector<int> ans = ob.printCousins(root, p);
+
+		for (int i = 0; i < ans.size(); i++)
+			cout << ans[i] << " ";
+
+		cout << "\n";
+
+
+	}
+
+	return 0;
+}
+
+
+// } Driver Code Ends
