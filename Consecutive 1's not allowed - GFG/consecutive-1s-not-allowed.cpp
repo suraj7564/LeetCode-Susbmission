7eaf06bf47b34335d84b25512261ca385a0b3be2
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-01-02 22:29:14 +0530

    Added solution - LeetHub

---
+++ Consecutive 1's not allowed - GFG/consecutive-1s-not-allowed.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// #define ll long long
+	ll countStrings(int n) {
+	    // code here
+	    ll a = 0, b = 1, mod = 1e9+7;
+	    for(int i=0;i<=n;i++){
+	        ll c = (a + b)%mod;
+	        a = b;
+	        b = c;
+	    }
+	    return b;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.countStrings(n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
