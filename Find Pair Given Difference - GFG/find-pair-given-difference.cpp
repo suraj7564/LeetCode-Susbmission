f6900e04bd5c33630a9aa12112cb540fb64fa376
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-01-04 10:28:40 +0530

    Added solution - LeetHub

---
+++ Find Pair Given Difference - GFG/find-pair-given-difference.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+ 
+using namespace std; 
+
+
+bool findPair(int arr[], int size, int n);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int l,n;
+        cin>>l>>n;
+        int arr[l];
+        for(int i=0;i<l;i++)
+            cin>>arr[i];
+        if(findPair(arr, l, n))
+            cout<<1<<endl;
+        else cout<<"-1"<<endl;
+    }
+    
+  
+    return 0;
+}
+// } Driver Code Ends
+
+
+bool findPair(int arr[], int size, int n){
+    //code
+    sort(arr, arr + size);
+    for(int i=size-1;i>=0;i--){
+        int idx = lower_bound(arr, arr + size, arr[i] - n) - arr;
+        if(arr[idx] == arr[i] - n && idx != i) return true;
+    }
+    return false;
+}
