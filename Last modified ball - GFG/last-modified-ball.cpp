d54c4b3264e2b1334f474779b12d486bfc366383
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-01-09 09:42:51 +0530

    Added solution - LeetHub

---
+++ Last modified ball - GFG/last-modified-ball.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int solve(int N, vector<int> A) {
+        // code here
+        int ans = N;
+        for(int i=N-1;i>=1;i--){
+            if(A[i] != 9) break;
+            ans = i;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        vector<int> A(N);
+
+        for (int i = 0; i < N; i++) cin >> A[i];
+
+        Solution ob;
+        cout << ob.solve(N, A) << "\n";
+    }
+    return 0;
+}
+    
+// } Driver Code Ends
