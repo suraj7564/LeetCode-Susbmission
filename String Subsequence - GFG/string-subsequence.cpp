a20d20989f3ea85b5107a8be54d4835431dbc2ea
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-01-12 23:28:02 +0530

    Added solution - LeetHub

--- String Subsequence - GFG/string-subsequence.cpp
+++ String Subsequence - GFG/string-subsequence.cpp
@@ -19,9 +19,45 @@ public:
         }
         return ans;
     }
-    int countWays(string S1, string S2){
+    int countWays(string a, string b){
         // code here
-        int cnt = get(0, 0, S1, S2);
+        //int cnt = get(0, 0, a, b);
+        
+        int m = a.length();
+        int n = b.length();
+     
+        // Create a table to store results of sub-problems
+        int DP[m + 1][n + 1] = { { 0 } };
+     
+        // If first string is empty
+        for (int i = 0; i <= n; ++i)
+            DP[0][i] = 0;
+     
+        // If second string is empty
+        for (int i = 0; i <= m; ++i)
+            DP[i][0] = 1;
+     
+        // Fill lookup[][] in bottom up manner
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                // If last characters are same, we have two
+                // options -
+                // 1. consider last characters of both strings
+                //    in solution
+                // 2. ignore last character of first string
+                if (a[i - 1] == b[j - 1])
+                    DP[i][j] = DP[i - 1][j - 1] + DP[i - 1][j];
+                     
+                else
+                    // If last character are different, ignore
+                    // last character of first string
+                    DP[i][j] = DP[i - 1][j];
+            }
+        }
+     
+        return DP[m][n];
     }
 };
 
