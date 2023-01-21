f5c2a1c993c963c6aefc57b3bbe6fc01c3d3decc
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-01-21 12:52:09 +0530

    Added solution - LeetHub

--- Minimum X (xor) A - GFG/minimum-x-xor-a.cpp
+++ Minimum X (xor) A - GFG/minimum-x-xor-a.cpp
@@ -1,42 +1,49 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 // Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 // User function Template for C++
 
 class Solution {
   public:
+    int cnt(int n){
+        int x = 0;
+        while(n){
+            if(n%2) x++;
+            n /= 2;
+        }
+        return x;
+    }
     int minVal(int a, int b) {
-        int ans=0;
-        vector<int> v(31,0);
-        int cnt=0;
-        for(int i=0;i<31;i++){
-            if(a&(1<<i)) v[i]=1;
-            if(b&(1<<i)) cnt++;
-        }
-        for(int i=31;i>=0&&cnt>0;i--){
-            if(v[i]==1){
-                cnt--;
-                v[i]=2;
+        // code here
+        int cnta = cnt(a), cntb = cnt(b);
+        int x = a;
+        if(cnta <= cntb){
+            for(int i=0;i<31 && cnta < cntb;i++){
+                int p = (1<<i);
+                if((x & p) == 0){
+                    cnta++;
+                    x |= p;
+                }
             }
         }
-        for(int i=0;i<31;i++){
-            if(v[i]==2) ans=(ans|(1<<i));
-        }
-        for(int i=0;i<31&&cnt>0;i++){
-            if(v[i]==0){
-                cnt--;
-                ans=ans|(1<<i);
+        else{
+            for(int i=0;i<31 && cnta > cntb;i++){
+                int p = (1<<i);
+                if((x & p) != 0){
+                    cnta--;
+                    x ^= p;
+                }
             }
         }
-        return ans;
+        return x;
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main() {
     int t;
@@ -52,4 +59,5 @@ int main() {
     }
 
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
