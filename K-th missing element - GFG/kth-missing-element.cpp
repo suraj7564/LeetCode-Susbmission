9912e90a6560a6d4a217335aa7477068f8c24b40
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-01-31 21:05:46 +0530

    Added solution - LeetHub

--- K-th missing element - GFG/kth-missing-element.cpp
+++ K-th missing element - GFG/kth-missing-element.cpp
@@ -30,26 +30,28 @@ int main()
 
 int KthMissingElement(int a[], int n, int k)
 {
-    // Complete the function
-    // 12 ...................  38 K = 5
-    //1, 3, 4, 6, 7 k = 2
-    // curAns = 0, i = 0, cnt = 0;
-    // step - 1 -> curAns = 1 , i = 1, cnt = 0
-    // step - 2 -> curAns = 2 , i = 1, cnt = 1;
-    // step - 3 -> curAns = 3 , i = 2, cnt = 1;
-    // step - 4 -> curAns = 4 , i = 3, cnt = 1;
-    // step - 5 -> curAns = 5 , i = 3, cnt = 2 = k return curAns
-    sort(a, a + n);
-    int cnt = 0, curAns = a[0], i = 0;
-    while(i < n && cnt < k){
-        if(curAns == a[i]){
-            i++;
-            curAns++;
-            continue;
+    int l = a[0], r = a[n - 1], ans = -1;
+    while(l <= r){
+        int m = (l + r)/2;
+        int idx = lower_bound(a, a+n, m) - a;
+        int total = m - a[0] + 1;
+        if(a[idx] != m) idx--;
+        // 5
+        //4 _ 6 _ _ _ 10 _ _ _ _ 15 _ _ _ _ 20
+        //1 3 4 5 7 k = 2
+        // l = 1, r = 7 
+        // m = 4, 4 idx 2, total = 4, present = 3, missing = 1
+        // l = 5, r = 7
+        // m = 6, 7 idx 4, total = 7, pr = 5, missing = 2 ans = 6
+        int present = idx + 1, missing = total - present;
+        
+        if(missing >= k){
+            ans = m;
+            r = m - 1;
         }
-        cnt++;
-        if(cnt == k) return curAns;
-        curAns++;
+        else{
+            l = m + 1;
+        }
     }
-    return -1;
+    return ans;
 }
