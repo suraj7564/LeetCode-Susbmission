9cd963923f18fce9c2410ad7785a506f449c43b2
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-02-02 13:06:11 +0530

    Added solution - LeetHub

--- Merge Sort - GFG/merge-sort.cpp
+++ Merge Sort - GFG/merge-sort.cpp
@@ -1,4 +1,4 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <stdio.h>
 #include <bits/stdc++.h>
 using namespace std;
@@ -15,60 +15,50 @@ void printArray(int arr[], int size)
 }
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution
 {
     public:
     void merge(int arr[], int l, int m, int r)
     {
-         int l1=m-l+1;
-         int l2=r-m;
-         int a[l1];
-         int b[l2];
-         for(int i=0;i<l1;i++){
-             a[i]=arr[l+i];
-         }
-         for(int i=1;i<=l2;i++){
-             b[i-1]=arr[m+i];
-         }
-         int x=0,y=0;
-         int idx=l;
-         while(x<l1&&y<l2){
-             if(a[x]<=b[y]){
-                 arr[idx++]=a[x++];
-             }
-             else{
-                 arr[idx++]=b[y++];
-             }
-         }
-         while(x<l1){
-             arr[idx++]=a[x++];
-         }
-         while(y<l2){
-             arr[idx++]=b[y++];
-         }
-        //  cout<<"a:";
-        //  for(int i=0;i<l1;i++){
-        //      cout<<a[i]<<" ";
-        //  }
-        //  cout<<endl<<"b:";
-        //  for(int i=0;i<l2;i++){
-        //      cout<<b[i]<<" ";
-        //  }
-        //  cout<<endl;
+        vector<int> a;
+        int i = l, j = m+1;
+        while(i<=m && j<=r){
+            if(arr[i]<=arr[j]){
+                a.push_back(arr[i]);
+                i++;
+            }
+            else{
+                a.push_back(arr[j]);
+                j++;
+            }
+        }
+        while(i<=m){
+            a.push_back(arr[i]);
+            i++;
+        }
+        while(j<=r){
+            a.push_back(arr[j]);
+            j++;
+        }
+        for(int i=0;i<a.size();i++){
+            arr[l+i] = a[i];
+        }
     }
     public:
     void mergeSort(int arr[], int l, int r)
     {
-        if(l>=r) return;
-        int m=(l+r)/2;
-        mergeSort(arr,l,m);
-        mergeSort(arr,m+1,r);
-        merge(arr,l,m,r);
+        //code here
+        if(l<r){
+            int m = (l+r)/2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m+1, r);
+            merge(arr, l, m, r);
+        }
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 
 int main()
@@ -89,4 +79,5 @@ int main()
     printArray(arr, n);
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
