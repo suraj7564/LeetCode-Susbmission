255aaea6b431b8d02c27c51a510d80dddc0638d7
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-02-02 22:31:34 +0530

    Added solution - LeetHub

--- Binary Search - GFG/binary-search.cpp
+++ Binary Search - GFG/binary-search.cpp
@@ -1,47 +1,44 @@
-// { Driver Code Starts
-//Initial template for C++
+//{ Driver Code Starts
+// Initial template for C++
 
 #include <bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
-//User function template for C++
+// } Driver Code Ends
+// User function template for C++
 
-class Solution{
-public:
-    int binarysearch(int arr[], int n, int k){
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
         // code here
-        int i=0,j=n-1;
-        while(i<=j){
-            int m=(i+j)/2;
-            if(arr[m]==k) return m;
-            if(arr[m]<k) i=m+1;
-            else j=m-1;
+        int l = 0, r = n - 1;
+        while(l<=r){
+            int m = (l + r)/2;
+            if(arr[m] == k) return m;
+            if(arr[m] > k) r = m - 1;
+            else l = m + 1;
         }
         return -1;
     }
 };
 
-// { Driver Code Starts.
-int main()
-{
+//{ Driver Code Starts.
+int main() {
     int t;
-    cin>>t;
+    cin >> t;
 
-    while(t--)
-    {
+    while (t--) {
         int N;
-        cin>>N;
+        cin >> N;
         int arr[N];
-        for(int i=0;i<N;i++)
-            cin>>arr[i];
+        for (int i = 0; i < N; i++) cin >> arr[i];
         int key;
-        cin>>key;
+        cin >> key;
         Solution ob;
-        int found = ob.binarysearch(arr,N,key);
-        cout<<found<<endl;
+        int found = ob.binarysearch(arr, N, key);
+        cout << found << endl;
     }
 }
 
-  // } Driver Code Ends
+// } Driver Code Ends
