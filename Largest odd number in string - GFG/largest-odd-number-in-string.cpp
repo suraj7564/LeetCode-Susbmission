15a3a15172791fffe0179230bb78916a282d9198
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-02-04 22:24:44 +0530

    Added solution - LeetHub

---
+++ Largest odd number in string - GFG/largest-odd-number-in-string.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string maxOdd(string s) {
+        // your code here
+        string ans = "";
+        for(int i=0;i<s.length();i++){
+            if((s[i] - '0')%2){
+                ans = s.substr(0, i + 1);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+
+        cout << ob.maxOdd(s) << endl;
+    }
+}
+
+// } Driver Code Ends
