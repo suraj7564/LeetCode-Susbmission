08572696749e9310891a908bd0f447d404117c7b
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-02-07 11:08:03 +0530

    Added solution - LeetHub

---
+++ Count numbers containing 4 - GFG/count-numbers-containing-4.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int countNumberswith4(int N) {
+        // code here
+        int cnt = 0;
+        for(int i=4;i<=N;i++){
+            for(auto x:to_string(i)){
+                if(x == '4'){
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.countNumberswith4(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
