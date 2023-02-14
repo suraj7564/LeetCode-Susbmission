94d5d9426155bae3f28ac5cda06dbeedad185fa9
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-02-14 18:49:39 +0530

    Added solution - LeetHub

--- Form a palindrome - GFG/form-a-palindrome.cpp
+++ Form a palindrome - GFG/form-a-palindrome.cpp
@@ -1,27 +1,27 @@
 //{ Driver Code Starts
 //Initial template for C++
 
-#include<bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
 // } Driver Code Ends
 //User function template for C++
 
-class Solution{   
-public:
-    int findMinInsertions(string a){
-        // code here 
-        string b = a;
-        reverse(b.begin(), b.end());
-        int n = a.length();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(a[i - 1] == b[j - 1]){
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+class Solution{
+  public:
+    int countMin(string s){
+    //complete the function here
+        int n = s.length();
+        string s1 = s;
+        reverse(s1.begin(), s1.end());
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(s[i] == s1[j]){
+                    dp[i+1][j+1] = 1 + dp[i][j];
                 }
                 else{
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j]);
                 }
             }
         }
@@ -30,19 +30,17 @@ public:
 };
 
 //{ Driver Code Starts.
-
-
-
 int main(){
     int t;
-    cin>>t;
+    cin >> t;
     while(t--){
-        string S;
-        cin>>S;
+        string str;
+        cin >> str;
         Solution ob;
-        cout<<ob.findMinInsertions(S)<<endl;
+        cout << ob.countMin(str) << endl;
     }
-    return 0;
+return 0;
 }
+
 
 // } Driver Code Ends
