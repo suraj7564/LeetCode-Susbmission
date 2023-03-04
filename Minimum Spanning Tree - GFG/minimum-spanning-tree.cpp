f53926d1fcb8af161f7d8b74abb378885893cc70
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-03-04 11:16:49 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -1,61 +1,57 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 
-
+class DSU{
+    public:
+    vector<int> p;
+    DSU(){
+        for(int i=0;i<=2000;i++){
+            p.push_back(i);
+        }
+    }
+    int get(int x){
+        if(p[x] == x) return x;
+        return p[x] = get(p[x]);
+    }
+    bool join(int x, int y){
+        x = get(x);
+        y = get(y);
+        if(x != y){
+            p[x] = y;
+            return true;
+        }
+        return false;
+    }
+};
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-	int papa[100001];
-    int rank[100001];
-    void set(){
-        for(int i=0;i<100001;i++){
-            papa[i]=i;
-            rank[i]=0;
-        }
-    }
-    int findpapa(int x){
-        if(papa[x]==x) return x;
-        return papa[x]=findpapa(papa[x]);
-    }
-    void check(int x,int y){
-        x=findpapa(x);
-        y=findpapa(y);
-        if(rank[x]>=rank[y]){
-            papa[y]=x;
-            rank[x]++;
-        }
-        else{
-            papa[x]=y;
-            rank[y]++;
-        }
-    }
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        set();
-        vector<vector<int>> e;
+        // code here
+        vector<vector<int>> edg;
         for(int i=0;i<V;i++){
             for(auto x:adj[i]){
-                e.push_back({x[1],x[0],i});
-               // cout<<i<<" "<<x[0]<<" "<<x[1]<<endl;
+                edg.push_back({x[1], x[0], i});
             }
         }
-        sort(e.begin(),e.end());
-        int ans=0;
-        for(auto x:e){
-            if(findpapa(x[1])!=findpapa(x[2])){
-                check(x[1],x[2]);
-                ans+=x[0];
+        sort(edg.begin(), edg.end());
+        DSU d;
+        int ans = 0;
+        for(auto x:edg){
+            if(d.join(x[1], x[2])){
+                ans += x[0];
             }
         }
         return ans;
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 
 int main()
@@ -86,4 +82,5 @@ int main()
     return 0;
 }
 
-  // } Driver Code Ends
+
+// } Driver Code Ends
