30e0cc89b7e27accff837538bd94981921624e4d
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-03-06 12:52:29 +0530

    Added solution - LeetHub

--- Number of NGEs to the right - GFG/number-of-nges-to-the-right.cpp
+++ Number of NGEs to the right - GFG/number-of-nges-to-the-right.cpp
@@ -7,18 +7,42 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
+class FenwickTree{
+    public:
+    vector<int> t;
+    FenwickTree(){
+        t.assign(1e5 + 5, 0);
+    }
+    void add(int idx){
+        for(;idx <= 1e5;idx = (idx|(idx + 1))){
+            t[idx] += 1;
+        }
+    }
+    int get(int r){
+        int ans = 0;
+        for(;r>=0;r = (r & (r + 1)) - 1){
+            ans += t[r];
+        }
+        return ans;
+    }
+    int get(int l, int r){
+        return get(r) - get(l);
+    }
+};
 class Solution{
 public:
 
     vector<int> count_NGE(int n, vector<int> &arr, int queries, vector<int> &ind){
         //write your code here
+        vector<int> a(n);
+        FenwickTree tr;
+        for(int i=n-1;i>=0;i--){
+            a[i] = tr.get(arr[i], 1e5);
+            tr.add(arr[i]);
+        }
         vector<int> ans;
         for(auto x:ind){
-            int cnt = 0;
-            for(int j=x+1;j<n;j++){
-                if(arr[j] > arr[x]) cnt++;
-            }
-            ans.push_back(cnt);
+            ans.push_back(a[x]);
         }
         return ans;
     }
