f3a3d56c41d0740f356c8abad503e1d4c1c74bf6
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-03-06 12:48:05 +0530

    Added solution - LeetHub

---
+++ Number of NGEs to the right - GFG/number-of-nges-to-the-right.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+
+    vector<int> count_NGE(int n, vector<int> &arr, int queries, vector<int> &ind){
+        //write your code here
+        vector<int> ans;
+        for(auto x:ind){
+            int cnt = 0;
+            for(int j=x+1;j<n;j++){
+                if(arr[j] > arr[x]) cnt++;
+            }
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while(t--){
+        //Input
+
+       int n; cin >> n;
+       vector<int> v1(n);
+       for(int i = 0;i<n;i++) cin >> v1[i];
+        int q; cin >> q;
+        vector<int> v2(q);
+        for(int i = 0;i<q;i++) cin >> v2[i];
+
+        Solution obj;
+        vector<int> ans = obj.count_NGE(n,v1,q,v2);
+
+        for(int i = 0;i<ans.size();i++)
+            cout << ans[i] << " ";
+        cout << endl;
+
+        
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
