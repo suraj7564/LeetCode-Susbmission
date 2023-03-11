aff4be6143b2bc81f4d8e266787432a6b88664c5
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-03-11 15:32:40 +0530

    Added solution - LeetHub

--- Predecessor and Successor - GFG/predecessor-and-successor.cpp
+++ Predecessor and Successor - GFG/predecessor-and-successor.cpp
@@ -110,26 +110,21 @@ void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
 {
 
 // Your code goes here
-    if(!root) return;
-    if(root->key == key){
-        if(root->left){
-            Node* temp = root->left;
-            while(temp->right) temp = temp->right;
-            pre = temp;
+    Node *temp = root;
+    while(root) {
+        if(root->key > key) {
+            suc = root;
+            root = root->left;
         }
-        if(root->right){
-            Node* temp = root->right;
-            while(temp->left) temp = temp->left;
-            suc = temp;
+        else  root = root->right;         
+    }
+
+    root = temp;
+    while(root) {
+        if(root->key < key) {
+            pre = root;
+            root = root->right;
         }
-        return;
-    }
-    if(root->key<key){
-        pre = root;
-        findPreSuc(root->right,pre,suc,key);
-    }
-    else{
-        suc = root;
-        findPreSuc(root->left,pre,suc,key);
+        else  root = root->left;         
     }
 }
