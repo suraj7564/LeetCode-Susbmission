da9daffd59c62670450e1352b024797f0d051a39
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-03-27 12:36:25 +0530

    Added solution - LeetHub

---
+++ Combination Sum II - GFG/combination-sum-ii.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    void solve(vector<int>& c,int idx,int target,vector<int>& cur,vector<vector<int>>& ans){
+        if(target==0){
+            ans.push_back(cur);
+        }
+        if(target<=0) return;
+        for(int i=idx;i<c.size();i++){
+            if(i!=idx&&c[i]==c[i-1]) continue;
+            cur.push_back(c[i]);
+            solve(c,i+1,target-c[i],cur,ans);
+            cur.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<int> cur;
+        vector<vector<int>> ans;
+        solve(candidates,0,target,cur,ans);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> candidates(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> candidates[i];
+        }
+        // char marker;
+        // cin >> marker;
+
+        Solution obj;
+
+        vector<vector<int>> comb = obj.combinationSum2(candidates, k);
+        sort(comb.begin(), comb.end());
+        cout << "[ ";
+        for (int i = 0; i < comb.size(); i++) {
+            cout << "[ ";
+            for (int j = 0; j < comb[i].size(); j++) {
+                cout << comb[i][j] << " ";
+            }
+            cout << "]";
+        }
+        cout << " ]\n";
+
+        // cout << "\n~\n";
+    }
+    fclose(stdout);
+    return 0;
+}
+
+// } Driver Code Ends
