6dd75608c40c9376bfffa4cb1c189847d9498362
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-04-04 16:01:31 +0530

    Added solution - LeetHub

--- Maximum Connected group - GFG/maximum-connected-group.cpp
+++ Maximum Connected group - GFG/maximum-connected-group.cpp
@@ -8,54 +8,80 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
+
+class DSU{
+    public:
+    vector<int> p;
+    vector<int> rank;
+    DSU(int n){
+        for(int i=0;i<=n*n;i++){
+            p.push_back(i);
+            rank.push_back(1);
+        }
+    }
+    void connect(int a, int b){
+        a = find(a), b = find(b);
+        if(a != b){
+            if(rank[a] >= rank[b]){
+                rank[a] += rank[b];
+                p[b] = a;
+            }
+            else{
+                rank[b] += rank[a];
+                p[a] = b;
+            }
+        }
+    }
+    int find(int a){
+        if(p[a] == a) return a;
+        return p[a] = find(p[a]);
+    }
+};
+
 class Solution {
   public:
-    map<int, int> componentSize;
     bool isValid(int i, int j, int n){
         if(i < 0 || j < 0 || i >= n || j >= n) return false;
 
         return true;
     }
-    void dfs(int i, int j, int n, vector<vector<int>>& grid, int curColor){
-        if(!isValid(i, j, n) || grid[i][j] == curColor || grid[i][j] == 0) return;
-        grid[i][j] = curColor;
-        componentSize[curColor]++;
-        int d[] = {0, 1, 0, -1, 0};
-        for(int k=0;k<4;k++){
-            dfs(i + d[k], j + d[k + 1], n, grid, curColor);
-        }
-    }
     int MaxConnection(vector<vector<int>>& grid) {
         // code here
-        componentSize.clear();
         int n = grid.size();
-        int curColor = 2;
+        int d[] = {0, 1, 0, -1, 0};
+        DSU ds(n);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] != 1) continue;
-                dfs(i, j, n, grid, curColor++);
+                if(grid[i][j] == 1){
+                    for(int k=0;k<4;k++){
+                        int x = i + d[k], y = j + d[k + 1];
+                        if(isValid(x, y, n) && grid[x][y] == 1){
+                            ds.connect(i * n + j, x * n + y);
+                        }
+                    }
+                }
             }
         }
         int ans = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] != 0) continue;
-                int d[] = {0, 1, 0, -1, 0};
-                int cur = 1;
-                set<int> s1;
-                for(int k=0;k<4;k++){
-                    int x = i + d[k], y = j + d[k + 1];
-                    if(isValid(x, y, n) && grid[x][y] != 0){
-                        s1.insert(grid[x][y]);
+                if(grid[i][j] == 0){
+                    set<int> s1;
+                    for(int k=0;k<4;k++){
+                        int x = i + d[k], y = j + d[k + 1];
+                        if(isValid(x, y, n) && grid[x][y] == 1){
+                            s1.insert(ds.find(n * x + y));
+                        }
                     }
+                    int cur = 0;
+                    for(auto x:s1){
+                        cur += ds.rank[x];
+                    }
+                    ans = max(ans, cur + 1);
                 }
-                for(auto x:s1){
-                    cur += componentSize[x];
-                }
-                ans = max(ans, cur);
             }
         }
-        if(ans == 0) ans = n * n;
+        if(ans == 0) return n*n;
         return ans;
     }
 };
