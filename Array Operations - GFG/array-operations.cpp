d2def369f7142642d6076f8b9f8257da841ec206
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-05-12 10:05:02 +0530

    Added solution - LeetHub

---
+++ Array Operations - GFG/array-operations.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int arrayOperations(int n, vector<int> &arr) {
+        // code here
+        int ans = 0, cur = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i] == 0){
+                ans += cur;
+                cur = 0;
+            }
+            else{
+                cur = 1;
+            }
+        }
+        ans += cur;
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        
+        int n;
+        cin>>n;
+        
+        
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        
+        Solution obj;
+        int res = obj.arrayOperations(n, arr);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
