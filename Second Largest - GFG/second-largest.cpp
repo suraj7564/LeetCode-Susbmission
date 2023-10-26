424fcc62e1554fc09326453268233d68e2216b72
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-10-26 20:39:37 +0530

    Added solution - LeetHub

--- Second Largest - GFG/second-largest.cpp
+++ Second Largest - GFG/second-largest.cpp
@@ -11,17 +11,19 @@ public:
 	// largest elements
 	int print2largest(int arr[], int n) {
 	    // code here
-	    int mx = -1, smx = -1;
-	    for(int i=0;i<n;i++){
-	        if(arr[i] > mx){
-	            smx = mx;
-	            mx = arr[i];
-	        }
-	        else if(arr[i] < mx){
-	            smx = max(smx, arr[i]);
-	        }
-	    }
-	    return smx;
+	    int ans=-1;
+        for(int i=0;i<n;i++)
+        { 
+            if(arr[i]>ans)
+                ans=arr[i];
+        }
+        int second=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]!=ans)
+                second=max(second,arr[i]);
+        }
+        return second;
 	}
 };
 
