3c0566e3553a7b22618d75eb953765420bafb74f
Suraj Patel <68553950+suraj7564@users.noreply.github.com>
2023-12-25 12:41:28 +0530

    Added solution - LeetHub

--- Ways to sum to N - GFG/ways-to-sum-to-n.cpp
+++ Ways to sum to N - GFG/ways-to-sum-to-n.cpp
@@ -1,37 +1,36 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h> 
 using namespace std; 
   
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution
 {
   public:
     // function to count the total  
-    // number of ways to sum up to 'N' 
+    // number of ways to sum up to 'N'
+    int mod = 1e9 + 7;
+    int solve(int m, int N, int arr[], vector<int>& dp){
+        if(N == 0) return 1;
+        if(N < 0) return 0;
+        if(dp[N] != -1) return dp[N];
+        
+        dp[N] = 0;
+        for(int i=0;i<m;i++){
+            dp[N] = (dp[N] % mod + solve(m, N - arr[i], arr, dp) % mod) % mod;
+        }
+        return dp[N] % mod;
+    }
     int countWays(int arr[], int m, int N) 
     { 
-      //code here.
-      int mod = 1e9+7;
-      vector<int> dp(N+1,0);
-      dp[0] = 1;
-      for(int i=0;i<=N;i++){
-          for(int j=0;j<m;j++){
-              if(arr[j]<=i)
-              dp[i] = (dp[i]%mod + dp[i-arr[j]]%mod)%mod; 
-          }
-      }
-    //   for(int i=0;i<=N;i++){
-    //       cout<<dp[i]<<" ";
-    //   }
-    //   cout<<endl;
-      return dp[N];
+        vector<int> dp(N + 1, -1);
+        return solve(m, N, arr, dp);
     } 
     
 };
 
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main()
 {
@@ -47,4 +46,5 @@ int main()
 	        Solution ob;     
 	        cout<<ob.countWays(arr,m,n)<<endl;
 	    }
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
